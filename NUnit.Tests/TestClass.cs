3105afff8932e62535bc7a13d9e7d10238c9eade
Eden Reich <eden.reich@gmail.com>
2018-06-09 01:11:56 +0200

    first commit

---
+++ NUnit.Tests/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnit.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://www.google.com");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Quick startup with Selenium and NUnit
